2bcc1c766a4e264f2a91fb35daf58e9e1fd6a932
Luiz Felipe F M Costa <luiz@thenets.org>
2020-07-10 18:31:47 -0300

    First animation working

--- minish/minish.cpp
+++ minish/minish.cpp
@@ -7,12 +7,12 @@ int main()
 
 	InitWindow(WindowWidth, (int)WindowHeight, "Minish");
 
-	SetTargetFPS(60);
+	SetTargetFPS(10);
 
 	int scale = 4;
 
 	// 32x24
-	auto link_img = LoadImage("../assets/sprites/link.png");
+	auto link_img = LoadImage("../assets/sprites-assets/hero.png");
 	ImageResizeNN(&link_img, link_img.width * scale, link_img.height * scale);
 	Texture2D link = LoadTextureFromImage(link_img);
 	int link_frame = 0;
@@ -22,11 +22,12 @@ int main()
 		BeginDrawing();
 		ClearBackground(GRAY);
 
-
+		link_frame += 1;
+		link_frame = link_frame % 3;
 
 		DrawTexturePro(
 			link,
-			Rectangle{ (float)(12 * scale * link_frame), (float)(6 * scale), (float)(32 * scale), (float)(32 * scale) },
+			Rectangle{ (float)(0 * scale + (link_frame * 32 * scale)), (float)(0 * scale), (float)(32 * scale), (float)(32 * scale) },
 			Rectangle{ (float)(100 * scale), (float)(100 * scale), (float)(32 * scale), (float)(32 * scale) },
 			Vector2{ 0,0 },
 			0.0f,
